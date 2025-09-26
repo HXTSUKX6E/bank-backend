b48726b9b220d131f74ae85b15bd0450d8fc7a3a
CauseImFeeling <depayu52@gmail.com>
2025-09-26 18:07:58 +0500

    add client-controller and endpoints

---
+++ src/main/java/com/example/bank_backend/controller/ClientController.java
@@ -0,0 +1,68 @@
+package com.example.bank_backend.controller;
+
+import com.example.bank_backend.exception.NoClientsFoundException;
+import com.example.bank_backend.model.Client;
+import com.example.bank_backend.model.LegalForm;
+import com.example.bank_backend.service.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService)
+    {
+        this.clientService = clientService;
+    }
+
+    // Получить всех клиентов (поиск + фильтрация)
+    @GetMapping
+    public List<Client> getAllClients(@RequestParam(required = false) String name,
+            @RequestParam(required = false) LegalForm legalForm,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "asc") String direction
+    ) {
+       List<Client> clients= clientService.findAllClients(name, legalForm, sortBy, direction);
+        if (clients.isEmpty()) {
+            throw new NoClientsFoundException("Клиенты с указанными критериями не найдены");
+        }
+       return clients;
+    }
+
+    // Получить клиента по ID
+    @GetMapping("/{id}")
+    public Client getClientById(@PathVariable Long id) {
+        return clientService.findClientById(id);
+    }
+
+    // Создать нового клиента
+    @PostMapping
+    public Client createClient(@Valid @RequestBody Client client) {
+        return clientService.createClient(client);
+    }
+
+    // Обновить (изменить) клиента по ID
+    @PutMapping("/{id}")
+    public Client updateClient(@PathVariable Long id, @Valid @RequestBody Client clientDetails) {
+        return clientService.updateClient(id, clientDetails);
+    }
+
+    // Удалить клиента по ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
+        try {
+            clientService.deleteClient(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
